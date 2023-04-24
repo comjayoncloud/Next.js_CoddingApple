b1ef9a416fa083b310db3c348c9a56144af34f0d
jaewoo <comjayoncloud@gmail.com>
2023-04-24 18:35:48 +0900

    lecture 4 : Learned map function , done with homework

--- fresh/app/list/page.js
+++ fresh/app/list/page.js
@@ -1,14 +1,15 @@
 export default function List() {
-  const name = "jeong";
+  let 상품 = ["tomato", "pasta", "coconut"];
   return (
     <div>
       <h4 className="title">상품목록</h4>
-      <div className="food">
-        <h4>상품1 $40</h4>
-      </div>
-      <div className="food">
-        <h4>상품2 $40</h4>
-      </div>
+      {상품.map((a) => {
+        return (
+          <div className="food">
+            <h4>{a}</h4>
+          </div>
+        );
+      })}
     </div>
   );
 }
