c5236cc5b1faff414b4edbf263282aa50b0549f1
jaewoo <comjayoncloud@gmail.com>
2023-04-24 19:23:50 +0900

    lecture 5 : put in img

--- fresh/app/cart/payment/page.js
+++ fresh/app/cart/payment/page.js
@@ -1,3 +1,17 @@
-export default function payment() {
-  return <h1>결제페이지입니다</h1>;
+export default function Cart() {
+  return (
+    <div>
+      <h4 className="title">Cart</h4>
+      <div className="cart-item">
+        <p>상품명</p>
+        <p>$40</p>
+        <p>1개</p>
+      </div>
+      <div className="cart-item">
+        <p>상품명</p>
+        <p>$40</p>
+        <p>1개</p>
+      </div>
+    </div>
+  );
 }
--- fresh/app/list/page.js
+++ fresh/app/list/page.js
@@ -1,11 +1,23 @@
+import Image from "next/image";
+import img0 from "../../public/food0.png";
+import img1 from "../../public/food1.png";
+import img2 from "../../public/food2.png";
+
 export default function List() {
   let 상품 = ["tomato", "pasta", "coconut"];
+  //   console.log(이미지);
+  const a = img0;
+  const b = img1;
+  const c = img2;
   return (
     <div>
       <h4 className="title">상품목록</h4>
-      {상품.map((a) => {
+      {상품.map((a, i) => {
         return (
-          <div className="food">
+          <div className="food" key={i}>
+            {/* <Image className="food-img" src={`이미지${i}`} alt="설명" /> */}
+
+            <img className="food-img" src={c} alt="gg" />
             <h4>{a}</h4>
           </div>
         );
