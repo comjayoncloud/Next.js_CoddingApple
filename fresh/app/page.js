0627ffd0af8a5feaa0d8ef489b8f3bb41880b5a9
jaewoo <comjayoncloud@gmail.com>
2023-04-24 17:31:30 +0900

    lecture 2 : basic react grammer 4

--- fresh/app/page.js
+++ fresh/app/page.js
@@ -1,8 +1,13 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
+  const name = "jaewoo";
   return (
-   
-  )
+    <div>
+      <h4 className="title"> 애플프레쉬</h4>
+      <p className="title-sub">ㅇㅇㅇ</p>
+      {name}
+    </div>
+  );
 }
