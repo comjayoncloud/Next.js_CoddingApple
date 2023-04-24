205b69ceb1df0947f5f0bc5a126784855ddeaa25
jaewoo <comjayoncloud@gmail.com>
2023-04-24 17:54:49 +0900

    lecture 3 : Routing

---
+++ fresh/app/abc/def/page.js
@@ -0,0 +1,15 @@
+export default function def() {
+  const name = "def";
+  return (
+    <div>
+      <h3> def 페이지에요</h3>
+      <h4 className="title">상품목록</h4>
+      <div className="food">
+        <h4>상품1 $40</h4>
+      </div>
+      <div className="food">
+        <h4>상품2 $40</h4>
+      </div>
+    </div>
+  );
+}
--- fresh/app/page.js
+++ fresh/app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   const name = "jaewoo";
@@ -7,7 +8,6 @@ export default function Home() {
     <div>
       <h4 className="title"> 애플프레쉬</h4>
       <p className="title-sub">ㅇㅇㅇ</p>
-      {name}
     </div>
   );
 }
