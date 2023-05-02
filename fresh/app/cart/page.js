c4d88f2634bba648bbc9ae2b27638a61dc0534e5
jaewoo <comjayoncloud@gmail.com>
2023-05-02 22:03:13 +0900

    lecture 6 : component

--- fresh/app/cart/page.js
+++ fresh/app/cart/page.js
@@ -1,3 +1,22 @@
-export default function cart() {
-  return <h1>장바구니입니다</h1>;
+import age from "./data";
+
+export default function Cart() {
+  const age = age;
+  return (
+    <div>
+      <h4 className="title">Cart</h4>
+      <CartItem />
+      <CartItem />
+    </div>
+  );
+}
+
+function CartItem() {
+  return (
+    <div className="cart-item">
+      <p>상품명</p>
+      <p>$40</p>
+      <p>1개</p>
+    </div>
+  );
 }
