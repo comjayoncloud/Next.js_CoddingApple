64f2ce7c8b32e353998edace39150f695b4b9fa8
jaewoo <comjayoncloud@gmail.com>
2023-05-03 12:23:31 +0900

    lecture 7 : props

--- fresh/app/cart/page.js
+++ fresh/app/cart/page.js
@@ -1,22 +1,28 @@
 // import age from "./data";
 import Hello from "./hello";
 export default function Cart() {
+  let m = ["TOMATO", "PASTA"];
   return (
     <div>
       <h4 className="title">Cart</h4>
-      <CartItem />
-      <CartItem />
+      <CartItem item={m[0]} />
+      <CartItem item={m[1]} />
       <Hello />
+      <Banner content="롯데카드" />
+      <Banner content="현대카드" />
     </div>
   );
 }
 
-function CartItem() {
+function CartItem(props) {
   return (
     <div className="cart-item">
-      <p>상품명</p>
+      <p>{props.item}</p>
       <p>$40</p>
       <p>1개</p>
     </div>
   );
+}
+function Banner(props) {
+  return <h5>{props.content} 결제 행사중</h5>;
 }
