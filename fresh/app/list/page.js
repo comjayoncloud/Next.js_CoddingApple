c08e05e1741fd76ed7c590cf8c30b0710a7ced9c
jaewoo <comjayoncloud@gmail.com>
2023-05-03 14:34:59 +0900

    lecture 9 : change array, object state . with deepcopy. done homework too

--- fresh/app/list/page.js
+++ fresh/app/list/page.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 
 export default function List() {
   let 상품 = ["tomato", "pasta", "coconut"];
-  let [number, setNumber] = useState(0);
+  let [number, setNumber] = useState([0, 0, 0]);
 
   return (
     <div>
@@ -14,17 +14,21 @@ export default function List() {
           <div className="food" key={i}>
             <img className="food-img" src={`/food${i}.png`} alt={`${i}`} />
             <h4>{a} #40</h4>
-            <span>{number}</span>
+            <span>{number[i]}</span>
             <button
               onClick={() => {
-                setNumber(number + 1);
+                let copy = [...number];
+                copy[i]++;
+                setNumber(copy);
               }}
             >
               +
             </button>
             <button
               onClick={() => {
-                setNumber(number - 1);
+                let copy = [...number];
+                copy[i]--;
+                setNumber(copy);
               }}
             >
               -
