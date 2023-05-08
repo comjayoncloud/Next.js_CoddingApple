02afc4b486adaff3d1432e5452cb7b094325f042
jaewoo <comjayoncloud@gmail.com>
2023-05-08 18:04:22 +0900

    lecture 12 : homework - map function

--- forum/app/list/page.js
+++ forum/app/list/page.js
@@ -3,21 +3,19 @@ import { connectDB } from "@/util/database.js";
 export default async function List() {
   let db = (await connectDB).db("forum");
   let result = await db.collection("post").find().toArray();
-
+  console.log(result);
+  let a = "a";
   return (
     <div className="list-bg">
-      <div className="list-item">
-        <h4>{result[0].title}</h4>
-        <p>1월 1일</p>
-      </div>
-      <div className="list-item">
-        <h4>글제목</h4>
-        <p>1월 1일</p>
-      </div>
-      <div className="list-item">
-        <h4>글제목</h4>
-        <p>1월 1일</p>
-      </div>
+      {result &&
+        result.map((data) => {
+          return (
+            <div className="list-item">
+              <h4>{data.title}</h4>
+              <p>{data.content}</p>
+            </div>
+          );
+        })}
     </div>
   );
 }
